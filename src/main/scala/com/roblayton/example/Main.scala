ed765beee3628e834864948738f1f41ed90a9f1a
Rob Layton <hire@roblayton.com>
2015-04-25 20:51:39 -0400

    Utilizes application.conf now

--- src/main/scala/com/roblayton/example/Main.scala
+++ src/main/scala/com/roblayton/example/Main.scala
@@ -1,16 +1,19 @@
 package com.roblayton.spray
 
 import akka.actor.ActorSystem
+
 import spray.routing.SimpleRoutingApp
 import spray.routing.Route
 import spray.http.MediaTypes
 
-object Main extends App with SimpleRoutingApp {
+import com.roblayton.example.config.Configuration
+
+object Main extends App with SimpleRoutingApp with Configuration {
   implicit var actorSystem = ActorSystem()
 
   var fragments = Fragment.fragments
 
-  startServer(interface = "localhost", port = 8080) {
+  startServer(interface = serviceHost, port = servicePort) {
     get {
       path("hello") {
         complete {
